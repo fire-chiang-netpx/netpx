8f85c092cfe9b17fee0fd44cfcb922d724e457b3
jiangcihuo <chiang-fire@outlook.com>
2020-06-04 16:56:31 +0800

    fix: 静态资源添加缓存

--- netpx-server/src/main/java/org/firecode/netpx/server/http/controller/StaticResourcesController.java
+++ netpx-server/src/main/java/org/firecode/netpx/server/http/controller/StaticResourcesController.java
@@ -3,6 +3,7 @@ package org.firecode.netpx.server.http.controller;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.firecode.netpx.server.Server;
@@ -10,6 +11,12 @@ import org.firecode.netpx.server.http.PathMatchingResourceClassResolver;
 import org.firecode.netpx.server.http.support.RequestMapping;
 import org.firecode.netpx.server.http.support.RequestMappingHandler;
 import org.firecode.netpx.server.http.support.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 
 import io.netty.handler.codec.http.FullHttpRequest;
 
@@ -20,6 +27,8 @@ import io.netty.handler.codec.http.FullHttpRequest;
 @RequestMapping(urls={StaticResourcesController.ROOT_STATIC_RESOURCES,PathMatchingResourceClassResolver.DEFAULT_PATH_SEPARATOR},method = RequestMethod.GET)
 public class StaticResourcesController implements RequestMappingHandler<String> {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(StaticResourcesController.class);
+	
 	public static final String ROOT_STATIC_RESOURCES = "/static";
 	
 	private static final String STATIC_RESOURCES_DIR = "webapp";
@@ -28,21 +37,28 @@ public class StaticResourcesController implements RequestMappingHandler<String> 
 	
 	private static final ClassLoader SERVER_CLASS_LOADER = Server.class.getClassLoader();
 	
+	private static final LoadingCache<String, String> RESOURCES_CACHE = CacheBuilder.newBuilder().maximumSize(150).expireAfterWrite(10, TimeUnit.MINUTES).build(new CacheLoader<String,String>() {
+        public String load(String uri) throws Exception {
+    		try(InputStream resourceInputStream = SERVER_CLASS_LOADER.getResourceAsStream(uri);){
+    			if(null != resourceInputStream) {
+    				try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resourceInputStream));) {
+    					String res = bufferedReader.lines().collect(Collectors.joining());
+    					return res;
+    				}
+    			}
+    		} catch(Exception e) {
+    			LOG.error("Static resources '"+uri+"' load fail", e);
+    		}
+    		return null;
+        }
+    });
+	
 	@Override
 	public String handler(FullHttpRequest req) throws Exception {
 		String uri = req.uri().replace(ROOT_STATIC_RESOURCES,STATIC_RESOURCES_DIR);
 		if(PathMatchingResourceClassResolver.DEFAULT_PATH_SEPARATOR.equals(uri) || STATIC_RESOURCES_DIR.equals(uri)) {
 			uri = INDEX_HTML_URI;
 		}
-		try(InputStream resourceInputStream = SERVER_CLASS_LOADER.getResourceAsStream(uri);){
-			if(null != resourceInputStream) {
-				try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resourceInputStream));) {
-					String res = bufferedReader.lines().collect(Collectors.joining());
-					return res;
-				}
-			}
-		}
-		return null;
+		return RESOURCES_CACHE.get(uri);
 	}
-
 }
